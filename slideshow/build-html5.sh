7f67a01033ba927263bbf25568d2cc3ecbf3344b
Krzysztof Sobkowiak <krzys.sobkowiak@gmail.com>
2016-07-23 23:03:34 +0200

    ALM and AP slides added

--- slideshow/build-html5.sh
+++ slideshow/build-html5.sh
@@ -4,9 +4,11 @@
 mkdir -p target
 rm -rf target/images
 rm -rf target/assets
+rm -rf target/slides
 rm -rf target/reveal.js
 cp -r src/docs/images target/
 cp -r src/docs/assets target/
+cp -r src/docs/slides target/
 cp -r .reveal.js/ target/reveal.js/
 echo "Building src/docs/idf-intro.adoc (html5)"
 asciidoctor -a allow-uri-read -b html5 -a toc2 -o target/idf-intro.html src/docs/idf-intro.adoc
--- slideshow/build-revealjs.sh
+++ slideshow/build-revealjs.sh
@@ -4,9 +4,11 @@
 mkdir -p target
 rm -rf target/images
 rm -rf target/assets
+rm -rf target/slides
 rm -rf target/reveal.js
 cp -r src/docs/images target/
 cp -r src/docs/assets target/
+cp -r src/docs/slides target/
 cp -r .reveal.js/ target/reveal.js/
 echo "Building src/docs/idf-intro.adoc (reveal.js)"
 asciidoctor -a allow-uri-read -b revealjs -T .asciidoctor-reveal.js/templates/slim -o target/idf-intro.html src/docs/idf-intro.adoc
